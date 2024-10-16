7035d8afa8e8efbdc26a15f78019f0b95956a2fa
ChenVand <chenvd@gmail.com>
2024-10-16 16:15:08 +0300

    partial patient_OT_assignment_for_day

--- src/assignment.rs
+++ src/assignment.rs
@@ -1,5 +1,5 @@
 use crate::builder::*;
-use std::collections::{HashMap, VecDeque, BTreeMap};
+use std::collections::{VecDeque, BTreeMap};
 
 pub struct Assignment<'a> {
     pub instance: &'a Instance,
@@ -7,10 +7,42 @@ pub struct Assignment<'a> {
     patients_per_day_per_surgeon: Vec<Vec<VecDeque<usize>>>,
 }
 
+//Implemented only for partitions of size 2
+pub struct SurgeonPartitionInfo<'b> {
+    pub patient_deq: &'b VecDeque<usize>,
+    pub total_duration: u16,
+    pub partition_location: usize,
+    pub partitioned_durations: (u16, u16),
+}
+
 impl<'a> Assignment<'a> {
 
-    //##### Change this to return dict according to surgeon idx
-    pub fn weighted_patient_by_surgeon_for_day (&self, day: usize) -> Vec<((usize, u16), VecDeque<(usize, u16)>)> {
+    //key: surgeon_index, value: (total_duration, deque<(patient_idx, duration)>)
+    pub fn get_surgeon_durations_partition_map (&self, day: usize) -> BTreeMap<usize, SurgeonPartitionInfo> {
+        let mut surgeon_durations_map = BTreeMap::new();
+        let get_partition_idx = |patient_deq: &VecDeque<usize>| {
+            patient_deq.len()/2
+        };
+
+        for surgeon_idx in 0..self.instance.surgeons.len() {
+            let patient_deq: &VecDeque<usize> =  &self.patients_per_day_per_surgeon[surgeon_idx][day];
+            let duration_vec: Vec<u16> = patient_deq.iter().map(|&idx| self.instance.patients[idx].surgery_duration).collect();
+            let partition_location = get_partition_idx(patient_deq);
+            surgeon_durations_map.insert(surgeon_idx, SurgeonPartitionInfo{
+                patient_deq,
+                total_duration: duration_vec.iter().sum(),
+                partition_location,
+                partitioned_durations: {
+                    (duration_vec.iter().enumerate().filter(|x| x.0 <= partition_location).map(|x| x.1).sum(),
+                    duration_vec.iter().enumerate().filter(|x| x.0 > partition_location).map(|x| x.1).sum())
+                }
+            });
+        }
+
+        surgeon_durations_map
+
+        //trash
+        /*
         let weighted_patients_per_surgeon: Vec<VecDeque<(usize, u16)>> = self.patients_per_day_per_surgeon.iter()
         .map(|surgeon_vec| surgeon_vec[day].iter().map(
         |&idx| (idx, self.instance.patients[idx].surgery_duration)).collect())
@@ -21,5 +53,6 @@ impl<'a> Assignment<'a> {
             .enumerate()
             .map(|(surgeon_idx, patients)| ((surgeon_idx, patients.iter().map(|x| x.1).sum::<u16>()), patients))
             .collect()
+        */
     }
 }
--- src/ot_and_room_assignment.rs
+++ src/ot_and_room_assignment.rs
@@ -3,7 +3,6 @@ use itertools::Itertools;
 use minilp::{Variable, Problem, OptimizationDirection, ComparisonOp};
 
 use crate::surgery_assignment;
-use crate::surgery_assignment::DayAssignment;
 use crate::builder::{Instance, Patient};
 use crate::assignment::Assignment;
 
@@ -11,7 +10,7 @@ use crate::assignment::Assignment;
 to re-assign from that day forward while keeping previous days intact.
 To do this, need to chose patient to bump. Recommended patient should have small duration, and long time to due date.*/
 
-pub fn assign_ots_and_rooms(instance: &Instance, day_assignment: &mut DayAssignment) {todo!();}
+// pub fn assign_ots_and_rooms(instance: &Instance, day_assignment: &mut DayAssignment) {todo!();}
 
 impl<'a> Assignment<'a> {
 
@@ -24,7 +23,7 @@ impl<'a> Assignment<'a> {
         let num_surgeons = instance.surgeons.len();
 
         // each surgeon is named and weighted: vec[i]: ((surgeon_idx, tot_duration), VecDeque<(patient_idx, duration)>)
-        let nw_patients_by_surgeon = self.weighted_patient_by_surgeon_for_day(day);
+        let surgeon_durations_partition_map = self.get_surgeon_durations_partition_map(day);
 
         // Set up bin_gradient
         struct Bin {
@@ -48,16 +47,16 @@ impl<'a> Assignment<'a> {
         //Surgeon variables X_{surgeon,bin}^(i,j) where j<=i
         let i_max = 1;
         let mut x_map: BTreeMap<(usize, usize, u8, u8), Variable> = BTreeMap::new();
-        for ((surgeon_idx, _), _) in &nw_patients_by_surgeon {
+        for surgeon_idx in 0..num_surgeons {
             for bin_idx in 0..num_bins {
                 for i in 0..=i_max {
                     for j in 0..=i {
                         if j==0 {
-                            x_map.insert((*surgeon_idx, bin_idx, i, j), 
+                            x_map.insert((surgeon_idx, bin_idx, i, j), 
                                 problem.add_var(i as f64 * instance.weights.surgeon_transfer + bin_gradient_weight(bin_idx),
                                 (0.0, 1.0)));
                         } else {
-                            x_map.insert((*surgeon_idx, bin_idx, i, j), 
+                            x_map.insert((surgeon_idx, bin_idx, i, j), 
                                 problem.add_var(bin_gradient_weight(bin_idx), (0.0, 1.0)));                
                         }
                     }
@@ -99,50 +98,40 @@ impl<'a> Assignment<'a> {
         }
 
         //Constraint \sum_{surgeon,i,j} {duration(s,i,j) * X_{surgeon, bin}^(i,j)} <= capacity(bin) * Y_bin
-        //Collect partitions for X^(1,_) (only make sense w.r.t nw_patients_by_surgeon). key: surgeon_idx, value: partition_idx
-        let partitions: BTreeMap<usize, usize> = BTreeMap::new();
-        //Only applies when i_max = 1:
-        let get_partition_idx = |patient_deq: VecDeque<(usize, u16)>| {
-            patient_deq.len()/2
-        };
-        let get_duration = |surgeon_idx: usize, i: u8, j: u8| {
-            if i==0 {
-                return 
-            }
-        };
         for bin_idx in 0..num_bins {
-            let summand = Vec::with_capacity((((i_max+1) * (i_max+2))/2) as usize * num_surgeons);
+            let mut summands = Vec::with_capacity((((i_max+1) * (i_max+2))/2) as usize * num_surgeons + 1);
             for surgeon_idx in 0..num_surgeons {
                 for i in 0..=i_max {
                     for j in 0..=i {
-                        summand.push((*x_map.get(&(surgeon_idx, bin_idx, i, j)).unwrap(), ))
+                        let duration: f64 = if i==0 { 
+                            surgeon_durations_partition_map.get(&surgeon_idx).unwrap().total_duration as f64
+                        } else if j==0 {
+                            surgeon_durations_partition_map.get(&surgeon_idx).unwrap().partitioned_durations.0 as f64
+                        } else if j==1 {
+                            surgeon_durations_partition_map.get(&surgeon_idx).unwrap().partitioned_durations.1 as f64
+                        } else {
+                            panic!("should not be able to reach here.");
+                        };
+                        
+                        summands.push((*x_map.get(&(surgeon_idx, bin_idx, i, j)).unwrap(), -duration));
                     }
                 }
             }
+            summands.push((*y_map.get(&bin_idx).unwrap(), instance.theaters[bin_idx].availability[day] as f64));
+            problem.add_constraint(summands, ComparisonOp::Ge, 0.0);
         }
-    
-        /*######################################
-        //Sort in decreasing tot_duration
-        nw_patients_by_surgeon.sort_by(|a,b| b.0.1.cmp(&a.0.1));
-    
-        let mut bins: Vec<(usize, u16)> = vec![];
-        for (idx, theater) in instance.theaters.iter().enumerate() {
-            bins.push((idx, theater.availability[day]));
-        }
-        //Sort in increasing weight
-        bins.sort_by(|a, b| a.1.cmp(&b.1));11
-    
-        let used_bins = Vec::<usize>::new();
-    
-        todo!();
-    
-        // biggest_in_biggest_bin_pack(&mut items, &mut bins)
-    
-        //##### If bin packer fails, call patient bumper */
+
+        //##### solve LP
+
+        //##### perform rounding
+
+        //##### if didn't work, return error so that patient will be bumped
+
     }
 }
 
-//##### Old 
+
+//biggest_in_biggest_bin_pack
 /*
 fn biggest_in_biggest_bin_pack(items: &mut Vec<(usize, u16)>, bins: &mut Vec<(usize, u16)>) -> Result<Vec<Vec<usize>>, String> {
     let mut bin_assignment: Vec<Vec<usize>> = vec![vec![]; bins.len()];
